4d727fc89b8b1a3fca8073fe034462c198f95ee7
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-05-21 12:20:35 +0530

    Truthy and Falsy values

--- script.js
+++ script.js
@@ -171,13 +171,39 @@
 
 //? type coercion
 
-console.log('I am ' + 19 + ' years old');
-console.log('I am ' + String(19) + ' years old');
-console.log('23' - '10' - 3);
-console.log('23' + '10' + 3);
-console.log('23' * '2');
-console.log('23' / '2');
+// console.log('I am ' + 19 + ' years old');
+// console.log('I am ' + String(19) + ' years old');
+// console.log('23' - '10' - 3);
+// console.log('23' + '10' + 3);
+// console.log('23' * '2');
+// console.log('23' / '2');
 
-let n = '1' + 1;   //11
-n = n - 1;
-console.log(n);
+// let n = '1' + 1;   //11
+// n = n - 1;
+// console.log(n);
+
+//? Truthy and Falsy values
+
+//5 Falsy values:= 0,'',undefined,null,NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+// console.log(Boolean('janu'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all;");
+}else{
+    console.log("You should get a job!");   
+}
+
+let height;
+if (height) {
+    console.log('YAY! Height is defined');
+}else{
+    console.log('Height is not define');
+}
