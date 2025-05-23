99ca07c6b6a1414efe196a7cea90a89ff0bc25df
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-05-23 12:33:14 +0530

    Functions

--- script2.js
+++ script2.js
@@ -2,14 +2,39 @@
  
  //? Activating Strict Mode
 
-let hasDriversLicence = false;
-const PassTest = true;
+// let hasDriversLicence = false;
+// const PassTest = true;
 
-if (PassTest) hasDriversLicence = true;
-if (hasDriversLicence) console.log('I can Drive');
+// if (PassTest) hasDriversLicence = true;
+// if (hasDriversLicence) console.log('I can Drive');
 
 //* All words is preserve
 // const interface = 'Audio';
 // const private = 534;
 // const  if = 23;
 
+
+//? Functions
+
+// function logger() {
+//     console.log('My name is Janu');   
+// }
+
+//calling / running /invoking function
+// logger();
+// logger();
+// logger(); 
+
+// function fruitProcessor(apples,oranges){
+//     console.log(apples,oranges);
+//     const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+//fruitProcessor(5,0,);
+
+// const appleJuice = fruitProcessor(5,0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5,0));
+
+// const appleOrangeJuice = fruitProcessor(2,4);
+// console.log(appleOrangeJuice);
