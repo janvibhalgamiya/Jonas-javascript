80ac06e34e368109c79ffcd17c90c2b1ac50a6a3
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-05-23 10:57:31 +0530

    Activating Strict Mode

---
+++ script2.js
@@ -0,0 +1,15 @@
+ 'use strict';
+ 
+ //? Activating Strict Mode
+
+let hasDriversLicence = false;
+const PassTest = true;
+
+if (PassTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log('I can Drive');
+
+//* All words is preserve
+// const interface = 'Audio';
+// const private = 534;
+// const  if = 23;
+
