e2e1c388fbb02b2497c0013e6e0e2708c9427f7d
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-05-28 16:43:03 +0530

    Looping Backwards and Loops in loops

--- script2.js
+++ script2.js
@@ -328,3 +328,29 @@
 //     if(typeof jonasArray[i] === 'number') break; 
 //     console.log(jonasArray[i],typeof jonasArray[i]); 
 // } 
+
+
+//? Looping Backwards and Loops in loops
+
+// const jonas = [
+//     'Janvi',
+//     'Prajapati',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael','Peter','Steven'],
+// ];
+
+// 0,1,...,4
+//4,3....,0
+
+// for(let i = jonas.length - 1; i >= 0 ; i--){
+//     console.log(i,jonas[i]);
+// }
+
+// for(let exercise = 1; exercise < 4; exercise++){
+//     console.log(`---------------Starting Exercise ${exercise}`);
+
+//     for(let rep = 1; rep < 6; rep++){
+//         console.log(`Exercise ${exercise} : Lifting weight repetition ${rep}`);
+//     }
+// }
