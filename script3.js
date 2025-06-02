0c4a93b4f5e0ab3c562027ec3cffe780017c946e
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-06-02 15:10:48 +0530

    Guess my number

--- script3.js
+++ script3.js
@@ -1,13 +1,4 @@
-//Remember, we're gonna use strict mode in all scripts now! 
-
 "use strict";
  
-
-
-const x = 23;
-if (x === 23) console.log(23);
-
-const calcAge = (birthYear) => 2037 - birthYear;
-
-console.log(calcAge());
+console.log(document.querySelector('.message').textContent  );
 
