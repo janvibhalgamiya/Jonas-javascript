51ed76cff468b8d0ff37994dea2bb3a97547e1ea
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-06-05 12:15:07 +0530

    Implementing HighScores

--- script3.js
+++ script3.js
@@ -15,6 +15,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20 ) +  1;
 let score = 20;
+let highscore = 0;
 
 
 
@@ -39,6 +40,11 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('body').style.backgroundColor = '#60b347';
         
         document.querySelector('.number').style.width = '30rem';
+
+        if(score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore; 
+        }
         
         //when guess is too high
     }else if(guess > secretNumber){
@@ -77,6 +83,7 @@ document.querySelector('.again').addEventListener('click',
 
         document.querySelector('body').style.backgroundColor = '#222';
         document.querySelector('.number').style.width = '15rem';
+        document.querySelector('.highscore').textContent = 0;
     }
 )
 //  ****************************************************** 
@@ -92,3 +99,4 @@ document.querySelector('.again').addEventListener('click',
 // 4. Also restore the original background color (#222) and number width (15rem)
 
 // GOOD LUCK �
+
