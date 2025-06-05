3551f22d934af3dae512d78e82493e3e7e0aa0f6
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-06-05 10:59:28 +0530

    Manipulating CSS Styles

--- script3.js
+++ script3.js
@@ -25,10 +25,18 @@ document.querySelector('.check').addEventListener('click', function(){
 
     //  const guess = document.querySelector('.message').textContent = '🎉 Correct Number!';
 
+    //when there is no input
      if (!guess) {
         document.querySelector('.message').textContent = '⛔ No number!';
+
+        //when players wins
      }else if(guess === secretNumber){
         document.querySelector('.message').textContent = '🎉 Correct Number!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+
+        //when guess is too high
      }else if(guess > secretNumber){
         if(score > 1){
             document.querySelector('.message').textContent =
@@ -39,6 +47,8 @@ document.querySelector('.check').addEventListener('click', function(){
             document.querySelector('.message').textContent = '💥 You lost the game!';
             document.querySelector('.score').textContent = 0;
         }
+
+        //when guess is too low
      }else if(guess < secretNumber){
         if(score > 1){
             document.querySelector('.message').textContent =
