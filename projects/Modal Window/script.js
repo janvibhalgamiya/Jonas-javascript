a9b5cfcf87d52dc58f4da02bba8df5c94014a13b
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-06-06 11:57:01 +0530

    Project Modal Window

--- projects/Modal Window/script.js
+++ projects/Modal Window/script.js
@@ -1,11 +1,28 @@
-'use strict';
+"use strict";
 
- const modal = document.querySelector('.modal');
- const overlay = document.querySelector('.overlay');
- const btnCloseModal = document.querySelector('.close-modal');
- const btnOpenModal = document.querySelectorAll('.show-modal');
- console.log("🚀 ~ btnOpenModal:", btnOpenModal);
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnOpenModal    .length; i++) {        
-console.log("🚀 ~ btnOpenModal:", btnOpenModal[i])
+const openModal = function () {
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  console.log(
+    "🚀 ~ btnOpenModal:",
+    btnOpenModal[i].addEventListener("click", openModal)
+  );
 }
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
