56bae74d0399eba52eae689b7cb28be7dcfbccab
janvibhalgamiya <janvibhalgamiya2006@gmail.com>
2025-06-09 16:20:10 +0530

    Resetting the Game

--- projects/Pig-Game/script.js
+++ projects/Pig-Game/script.js
@@ -13,15 +13,27 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-//starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  //starting conditions
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -65,10 +77,10 @@ btnHold.addEventListener("click", function () {
       scores[activePlayer];
 
     //2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //Finish the game
       playing = false;
-      diceEl.classList.add('hidden');
+      diceEl.classList.add("hidden");
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add("player--winner");
@@ -81,3 +93,25 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+btnNew.addEventListener("click",init
+    // function () {
+  // Reset scores
+  //   score0El.textContent = 0;
+  //   score1El.textContent = 0;
+  //   current0El.textContent = 0;
+  //   current1El.textContent = 0;
+  //? Reset game state
+  //   scores[0] = 0;
+  //   scores[1] = 0;
+  //   currentScore = 0;
+  //   activePlayer = 0;
+  //   playing = true;
+  //? Reset UI
+  //   diceEl.classList.add("hidden");
+  //   player0El.classList.remove("player--winner");
+  //   player1El.classList.remove("player--winner");
+  //   player0El.classList.add("player--active");
+  //   player1El.classList.remove("player--active");
+//}
+);
